d076d4f479e86911f1126a2ab802849ad51205d1
s1903080 <j.xiao-17@sms.ed.ac.uk>
2020-04-13 09:18:19 +0100

    feat: first commit

---
+++ precache-manifest.63f2efc29945343d5887000f8ab981b6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987de17d3b3aa05be7d32fe7ffda7898",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2feb0fb40fdcfe8b75a",
+    "url": "/static/css/2.4af70dfe.chunk.css"
+  },
+  {
+    "revision": "11283a60559e53b9cdc8",
+    "url": "/static/css/main.cc808394.chunk.css"
+  },
+  {
+    "revision": "d2feb0fb40fdcfe8b75a",
+    "url": "/static/js/2.033a9788.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.033a9788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11283a60559e53b9cdc8",
+    "url": "/static/js/main.b7db53e3.chunk.js"
+  },
+  {
+    "revision": "1aae69bc559d1a21491f",
+    "url": "/static/js/runtime-main.d3377e40.js"
+  },
+  {
+    "revision": "b07e12ff81258aae9c9450f43fd1e913",
+    "url": "/static/media/avatar.b07e12ff.jpg"
+  },
+  {
+    "revision": "ee3a14e14d940152905b2aed39529510",
+    "url": "/static/media/brand_values.ee3a14e1.csv"
+  },
+  {
+    "revision": "d6109f6c1b2a040e53add7b6d3e12721",
+    "url": "/static/media/data.d6109f6c.csv"
+  },
+  {
+    "revision": "f342d81f1ef31509c59122908f276ff7",
+    "url": "/static/media/footer.f342d81f.png"
+  },
+  {
+    "revision": "f84ab0d325bdfe207491cb8c6e253fa5",
+    "url": "/static/media/home.f84ab0d3.png"
+  },
+  {
+    "revision": "09ca6e75cbc72f9ab6a3cff693db8b04",
+    "url": "/static/media/trump.09ca6e75.png"
+  }
+]);
